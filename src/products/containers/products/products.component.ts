1c76e130b04ff116c03afefa2e74b78b846f9442
Evgeniy Aksyonov <eaksyonov@cogniance.com>
2018-01-10 14:54:27 +0200

    Our first @Effect

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -27,7 +27,7 @@ import { Pizza } from '../../models/pizza.model';
         </pizza-item>
       </div>
     </div>
-  `,
+  `
 })
 export class ProductsComponent implements OnInit {
   pizzas$: Observable<Pizza[]>;
@@ -36,5 +36,6 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     this.pizzas$ = this.store.select(fromStore.getAllPizzas);
+    this.store.dispatch(new fromStore.LoadPizzas());
   }
 }
